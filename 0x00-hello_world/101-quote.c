cee94bd78f526e73c046a5b39afa0506e872fadb
evaniewares <evaniewares@gmail.com>
2023-06-15 14:30:37 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - A program that print a quote without using printf and puts
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char* quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int i = 0;
-
-	while(quote[i] != '\0')
-	{
-		putchar(quote[i]);
-		i++;
-	}
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
