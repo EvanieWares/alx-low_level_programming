955743a114b5aeaa5cfaf41dd5657494d4fb7750
evaniewares <evaniewares@gmail.com>
2023-06-21 01:11:03 +0200

    Fix in 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
+#define MAX_VALUE 10000000000
 
 /**
- * main - prints the first 98 Fibonacci numbers,
- * starting with 1 and 2
+ * print_fibonacci - prints the fibonacci numbers
  *
- * Return: 0
+ * Return: void
+ */
+void print_fibonacci(void)
+{
+	int i;
+	unsigned long int previous = 1;
+	unsigned long int current = 2;
+
+	printf("%lu, %lu, ", previous, current);
+
+	for (i = 3; i <= 98; i++)
+	{
+		if (current > MAX_VALUE - previous)
+		{
+			unsigned long int least_significant = (previous + current) % MAX_VALUE;
+			unsigned long int remaining = (previous + current) / MAX_VALUE;
+			unsigned long int temp = current;
+
+			current = least_significant;
+			previous = temp / MAX_VALUE + remaining;
+
+			printf("%lu%010lu", previous, current);
+		}
+		else
+		{
+			unsigned long int temp = current;
+
+			current = previous + current;
+			previous = temp;
+
+			printf("%lu", current);
+		}
+
+		if (i != 98)
+		{
+			printf(", ");
+		}
+	}
+}
+
+/**
+ * main - a program thet prints the first 98
+ * fibonacci numbers
+ *
+ * Return: void
  */
 int main(void)
 {
-	int max = 98;
-	int i;
-	double first = 1;
-	double second = 2;
-	double third;
+	print_fibonacci();
 
-	printf("%.0f, %.0f", first, second);
-
-	for (i = 3; i <= max; i++)
-	{
-		printf(", ");
-		third = first + second;
-		printf("%.0f", third);
-		first = second;
-		second = third;
-	}
 	printf("\n");
+
 	return (0);
 }
