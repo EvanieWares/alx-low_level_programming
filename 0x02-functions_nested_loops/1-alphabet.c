e74b75fed7c5fa5fc18bf884514c75c45f4223b5
evaniewares <evaniewares@gmail.com>
2023-06-20 05:20:20 -0700

    Fixed 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,5 @@
 #include "main.h"
 
-void print_alphabet(void);
-
-/**
- * main - A program that prints alphabet letters
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
  * print_alphabet - a function that prints alphabet letters
  *
