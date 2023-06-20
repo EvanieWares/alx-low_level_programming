6b3ebf9539f59ecbcdc8d6a161b6b7f6cdc69eaa
evaniewares <evaniewares@gmail.com>
2023-06-20 09:42:20 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.c"
+
+/**
+ * add - adds two integers
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
