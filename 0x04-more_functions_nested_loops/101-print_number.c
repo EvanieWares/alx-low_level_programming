0e40568b3b8624602aba4be9f49a9bc2619e1fc4
evaniewares <chisopsyelera@gmail.com>
2023-06-22 19:25:01 +0000

    Finally fixed a bug in 101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,9 +8,6 @@
 
 void print_number(int n)
 {
-	int digits;
-	int temp;
-	int div;
 
 	if (n < 0)
 	{
@@ -18,26 +15,12 @@ void print_number(int n)
 		_putchar('-');
 	}
 
-	if (n < 10)
+	if (n / 10)
 	{
-		_putchar(n + '0');
-		return;
+		print_number(n / 10);
 	}
 
-	div = 1;
-	digits = 1;
-	temp = n;
-	while (temp > 9)
-	{
-		div *= 10;
-		digits++;
-		temp /= 10;
-	}
+	_putchar(n % 10 + '0');
 
-	while (div > 0)
-	{
-		_putchar(n / div + '0');
-		n %= div;
-		div /= 10;
-	}
+	
 }
