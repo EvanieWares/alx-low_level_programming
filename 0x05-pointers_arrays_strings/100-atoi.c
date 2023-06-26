6d92c9904579de176a6f48fc7a9c337cd264e63e
evaniewares <chisopsyelera@gmail.com>
2023-06-26 19:48:01 +0000

    Fixed a bug in 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: char to check
+ * Return: 1 if true, 0 if false
+ */
+
 int _isdigit(char c)
 {
 	return (c >= '0' && c <= '9');
@@ -16,6 +22,7 @@ int _atoi(char *s)
 	int len = 0;
 	int result = 0;
 	int sign = 1;
+	int started = 0;
 
 	while (s[len] != '\0')
 	{
@@ -29,13 +36,21 @@ int _atoi(char *s)
 		}
 		else if (_isdigit(s[len]))
 		{
-			result = result * 10 + (s[len] - '0');
+			started = 1;
+			if (sign > 0)
+			{
+				result = result * 10 + (s[len] - '0');
+			}
+			else
+			{
+				result = result * 10 - (s[len] - '0');
+			}
 		}
-		else if (result > 0)
+		else if (started && s[len] == ' ')
 		{
 			break;
 		}
 		len++;
 	}
-	return (result * sign);
+	return (result);
 }
