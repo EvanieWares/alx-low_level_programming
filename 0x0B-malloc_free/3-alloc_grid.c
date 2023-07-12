6349c9513341ca22e2e33c4b8634c638a55c0e30
evaniewares <chisopsyelera@gmail.com>
2023-07-12 22:13:12 +0000

    Fixed 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,8 +26,8 @@ int **alloc_grid(int width, int height)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			array[i][j] = 0;
 	return (array);
 }
