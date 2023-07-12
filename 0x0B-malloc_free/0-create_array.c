2e2e4e76c13623b51de8854d2a137e2e0961c511
evaniewares <chisopsyelera@gmail.com>
2023-07-12 18:36:55 +0000

    Fixed 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str;
 
-	if (size == 0)
-		return (NULL);
-
 	str = (char *) malloc(size);
+	if (str == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
