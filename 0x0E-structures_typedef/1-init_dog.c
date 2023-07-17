bf0e93d00b5f09ebfbc61cb097495a9f870aff2e
evaniewares <evaniewares@gmail.com>
2023-07-17 05:45:19 -0700

    fixed 1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,7 @@
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
- * 
+ *
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
