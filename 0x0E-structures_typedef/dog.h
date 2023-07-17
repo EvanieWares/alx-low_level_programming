2e3bb344985b16e30d590bf088c815f46a2421b7
evaniewares <evaniewares@gmail.com>
2023-07-17 06:11:50 -0700

    ok

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-int _putchar (char c);
-
 /**
  * struct dog - data structure
  * @name: name of dog
@@ -17,11 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
