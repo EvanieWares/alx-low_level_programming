90d1b1a5215b098a082408a72f92a59330e1b4fe
evaniewares <chisopsyelera@gmail.com>
2023-09-14 16:46:32 +0000

    mandatory

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -22,5 +22,5 @@ size_t dlistint_len(const dlistint_t *h)
 		node = node->next;
 	}
 
-	return (count);	
+	return (count);
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,13 +2,14 @@
 
 /**
  * free_dlistint - frees a dlistint_t list
- * @hear: pointer to the head node
+ * @head: pointer to the head node
 */
 void free_dlistint(dlistint_t *head)
 {
 	while (head)
 	{
 		dlistint_t *temp = head;
+
 		head = head->next;
 		free(temp);
 	}
