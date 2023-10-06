072a8460c0bfd9c8e68c6b492e5d55ea61478d96
evaniewares <chisopsyelera@gmail.com>
2023-10-06 17:45:06 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,10 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	hash_table->array = malloc(size * sizeof(hash_node_t));
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
 	hash_table->size = size;
